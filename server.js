46aa97762362a17df5b15875b64409fac5e6f63b
MrV01 <VladV454@gmail.com>
2017-11-14 15:37:20 -0500

    Prep repository for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 //  Create our app
 var app = express();
+const  PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Heroku receives traffic via HTTPS.
+// app.js listens HTTP . Therefore :
+//  Forward https traffic to http, Using express midlleware
+//  parameters:  ( request, response, what to do next )
+app.use( function (req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+      next();      // nothing to do , call next()
+    } else {  // redirect to HTTP listener.
+      res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
-app.listen(3000, function () {
-  console.log('Express server is up on the port 3000');
+app.use(express.static('dist'));
+
+app.listen(PORT, function () {
+  console.log('Express server is up on the port ' + PORT);
 });
