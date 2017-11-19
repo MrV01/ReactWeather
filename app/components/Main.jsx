c3f275c549433e0d86a5dc646449f4d6a8a2ccf3
MrV01 <VladV454@gmail.com>
2017-11-19 16:01:41 -0500

    Main.jsx introduces Float Grid from Foundation (legacy)

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,7 +4,7 @@ var React = require('react');
 var Nav = require('Nav');
 
 // Import sampleImage
-import sampleImage from '../assets/images/sampleimage.jpg';
+var sampleImage  = require( '../assets/images/sampleimage.jpg');
 
 // var Main = React.createClass({
 //   render: function () {
@@ -26,8 +26,14 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main Component</h2>
-      {props.children}
+      {/* Including  Legacy Float Grid */}
+      <div className="row">
+        {/* medium screen = 6 columns out of 12, large =3 /12 , small = centered */}
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
+      {/* My little extra.  */}
       <img src={ sampleImage } alt='Sample Image' />
       <p className="sample__paragraph">lorem ipsum Semple text</p>
     </div>
